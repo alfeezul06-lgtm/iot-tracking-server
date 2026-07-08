78ac39cc3cb03efb040564fb0a398e2782dc49dc
alfeezul06-lgtm <alfeezul06@gmail.com>
2026-07-09 02:53:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.delete('/remove/:id', (req, res) => {
 
 // --- 🌐 FALLBACK ROUTE ---
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'));
+    res.sendFile(path.join(__dirname, 'public',));
 });
 
 app.listen(PORT, () => {
