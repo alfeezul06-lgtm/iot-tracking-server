0de0d50b7c52caa655e9aa1ea8602498c8a5b0c4
alfeezul06-lgtm <alfeezul06@gmail.com>
2026-07-09 01:39:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join(__dirname, 'package.json');
+const DATA_FILE = path.join(__dirname, 'public');
 
 // Middleware
 app.use(cors()); // Allows your GitHub Pages / frontend to connect without CORS errors
